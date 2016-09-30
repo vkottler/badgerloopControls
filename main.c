9ac03a92d852e128177bafd980933ad0e06207c6
Nick Beckwith <nbeckwith2@wisc.edu>
2016-09-29 23:06:41 -0500

    Added Sesnor structs and modularity
    Created a sensor struct that stores the pins of interest as well as the
    type. That type is used as a paramater for our general readSensor
    function which can take a sensor and output the correct value. I'm
    looking for better ways to import struct data.

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <xc.h>
 #include "main.h"
 
-#define MAX_LENGTH      100
+#define MAX_LENGTH          100
+#define NUM_SENSORS         15
+#define THERMISTOR          1
+#define VOLTAGE             4
+#define RAW_VALUE           0
+
+/* having a sensors struct should make formatting to a csv easier since we
+ can just write a for loop to output all the data*/
+struct Sensor {
+    char* typeName;
+    // make sure type corresponds to the correct constant
+    int type;
+    int pin1;
+    int pin2;
+};
 
 void initializers(void) {
    
@@ -9,31 +23,63 @@ void initializers(void) {
     initUART();
     ADCinit();
 }
+/* In order to make this more modular */
+int readSensor(struct Sensor sensor) {
+    switch(sensor.type) {
+        case THERMISTOR :
+            return readThermistor(sensor.pin1, sensor.pin2);
+            break;
+         // add extra cases for each sensor
+        case RAW_VALUE :
+            return readADC(sensor.pin1);
+           
+    }
+}
+
 
 int main(void) {
     
     initializers();
+    struct Sensor sensors[NUM_SENSORS];
+    // if there's a better way of doing this, please change
+    sensors[0]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[1]   =   (struct Sensor){"THERMISTOR 2", THERMISTOR, 0, 0};
+    sensors[2]   =   (struct Sensor){"THERMISTOR 3", THERMISTOR, 0, 0};
+    sensors[3]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[4]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[5]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[6]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[7]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[8]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[9]   =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[10]  =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[11]  =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[12]  =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[13]  =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[14]  =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
+    sensors[15]  =   (struct Sensor){"THERMISTOR 1", THERMISTOR, 0, 0};
     
-    int analogInput1=0;
-    int analogInput2=0; 
-    int analogInput3=0;
-    int analogInput4=0;
-    int analogInput5=0;
-    int analogInput6=0;
-    int analogInput7=0;
-    int analogInput8=0;
-    int analogInput9=0;
-    int analogInput10=0; 
-    int analogInput11=0;
-    int analogInput12=0;
-    int analogInput13=0;
-    int analogInput14=0;
-    int analogInput15=0;
-       
-    while (1) {
-      
-        char reading[100];
+    int i;
+    while (1) {   
+        for (i = 0; i < NUM_SENSORS; i++) {
+            // #clarity
+            printf("Reading %s: %d\n", sensors[i].typeName, readSensor(sensors[0]));
+        }
         
+        delay(1000, MILLI);
+    }
+    return 0;
+}
+
+  /*
+   * 
+   *    for (i = 0; i < NUM_ANALOG_INPUTS; i++) {
+        analogInputs[i] = 0;
+        }
+       
+        char reading[100];
+        char base []= "Reading 1 ";
+
         analogInput1=readADC(0);
         analogInput2=readADC(1);
         analogInput3=readADC(2);
@@ -49,19 +95,22 @@ int main(void) {
         analogInput13=readADC(12);
         analogInput14=readADC(13);
         analogInput15=readADC(14);
-   
-        char base []= "Reading 1 ";
        
-        sprintf(reading, "Reading 1: %d \n Reading 2: %d \n Reading 3: %d \n Reading 4: %d \n Reading 5: %d \n Reading 6: %d \n Reading 7: %d \n Reading 8:  %d \n", (int)analogInput1, (int)analogInput2, (int)analogInput3, (int)analogInput4, (int)analogInput5, (int)analogInput6, (int)analogInput7, (int)analogInput8);
+        sprintf(reading, "Reading 1: %d \n Reading 2: %d \n Reading 3: %d \n "
+                "Reading 4: %d \n Reading 5: %d \n Reading 6: %d \n Reading 7: "
+                "%d \n Reading 8:  %d \n", (int)analogInput1, (int)analogInput2,
+                (int)analogInput3, (int)analogInput4, (int)analogInput5, 
+                (int)analogInput6, (int)analogInput7, (int)analogInput8);
+        
         print(reading);
-        sprintf( reading, "Reading 9: %d \n Reading 10: %d \n Reading 11: %d \n Reading 12:  %d \n Reading 13: %d \n Reading 14: %d \n Reading 15: %d \n",(int)analogInput9, (int)analogInput10, (int)analogInput11, (int)analogInput12, (int)analogInput13, (int)analogInput14, (int)analogInput15);
+        sprintf( reading, "Reading 9: %d \n Reading 10: %d \n Reading 11: %d \n"
+                " Reading 12:  %d \n Reading 13: %d \n Reading 14: %d \n Reading"
+                " 15: %d \n", (int)analogInput9, (int)analogInput10, 
+                (int)analogInput11, (int)analogInput12, (int)analogInput13, 
+                (int)analogInput14, (int)analogInput15);
         print(reading);
         
         println("");
        
         delay(1000, MILLI);
-     
-   
-    }
-    return 0;
-}
+*/
