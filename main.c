52fa115481c3ae512c5fb964d5c13826781e4798
echan7 <echan7@wisc.edu>
2016-10-04 10:25:44 -0700

    scale factors and 1 pin sensor
    not sure for the pressure but all analog only needs 1 pin

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 
 #define MAX_LENGTH          100
 #define NUM_SENSORS         15
-#define THERMISTOR          1
-#define LEM                 2
-#define VTAPS               3
-#define PRESSURE            4
+#define THERMISTOR_REGULAR  1
+#define THERM_REGULAR_SCALE 1111
+#define THERMISTOR_MOTOR    2
+#define THERM_MOTOR_SCALE   1111
+#define LEM                 3
+#define LEM_SCALE           1111
+#define VTAPS               4
+#define VTAPS_SCALE         1111
+#define PRESSURE            5
 #define RAW_VALUE           0
 
 /* having a sensors struct should make formatting to a csv easier since we
@@ -15,8 +20,8 @@ struct Sensor {
     char* name;
     // make sure type corresponds to the correct constant
     int type;
-    int pin1;
-    int pin2;
+    int pin;
+    
 };
 
 void initializers(void) {
@@ -28,18 +33,28 @@ void initializers(void) {
 /* In order to make this more modular */
 double readSensor(struct Sensor sensor) {
     switch(sensor.type) {
-        case THERMISTOR :
-            return readThermistor(sensor.pin1, sensor.pin2);
+        case THERMISTOR_REGULAR :
+            return readADC(sensor.pin)*THERM_REGULAR_SCALE;
+            break;
+        case THERMISTOR_MOTOR:
+            return readADC(sensor.pin)*THERM_MOTOR_SCALE;
             break;
         case LEM :
+            return readADC(sensor.pin)*LEM_SCALE;
             break;
         case VTAPS :
+            return readADC(sensor.pin)*VTAPS_SCALE;
             break;
          // add extra cases for each sensor
         case PRESSURE :
             return HPgetPressure();
+            break;
         case RAW_VALUE :
-            return readADC(sensor.pin1);
+            return readADC(sensor.pin);
+            break;
+        default:
+            return readADC(sensor.pin);
+            break;
            
     }
 }
@@ -51,22 +66,22 @@ int main(void) {
     struct Sensor sensors[NUM_SENSORS];
     // if there's a better way of doing this, please change
     // var name  =   {type for print, type for double, pin1, pin2}
-    sensors[0]   =   (struct Sensor){"MOTOR THERMISTOR 1", THERMISTOR, 0, 0};
-    sensors[1]   =   (struct Sensor){"MOTOR THERMISTOR 2", THERMISTOR, 0, 0};
-    sensors[2]   =   (struct Sensor){"PRESSURE SENSOR", PRESSURE, 0, 0};
-    sensors[3]   =   (struct Sensor){"THERMISTOR 2", THERMISTOR, 0, 0};
-    sensors[4]   =   (struct Sensor){"THERMISTOR 3", THERMISTOR, 0, 0};
-    sensors[5]   =   (struct Sensor){"THERMISTOR 4", THERMISTOR, 0, 0};
-    sensors[6]   =   (struct Sensor){"THERMISTOR 5", THERMISTOR, 0, 0};
-    sensors[7]   =   (struct Sensor){"THERMISTOR 6", THERMISTOR, 0, 0};
-    sensors[8]   =   (struct Sensor){"THERMISTOR 7", THERMISTOR, 0, 0};
-    sensors[9]   =   (struct Sensor){"THERMISTOR 8", THERMISTOR, 0, 0};
-    sensors[10]  =   (struct Sensor){"THERMISTOR 9", THERMISTOR, 0, 0};
-    sensors[11]  =   (struct Sensor){"LEM 1", LEM, 0, 0};
-    sensors[12]  =   (struct Sensor){"LEM 2", LEM, 0, 0};
-    sensors[13]  =   (struct Sensor){"VTAPS 1", VTAPS, 0, 0};
-    sensors[14]  =   (struct Sensor){"VTAPS 2", VTAPS, 0, 0};
-    sensors[15]  =   (struct Sensor){"VTAPS 3", VTAPS, 0, 0};
+    sensors[0]   =   (struct Sensor){"MOTOR THERMISTOR 1", THERMISTOR_MOTOR, 0};
+    sensors[1]   =   (struct Sensor){"MOTOR THERMISTOR 2", THERMISTOR_MOTOR, 1};
+    sensors[2]   =   (struct Sensor){"THERMISTOR 9", THERMISTOR_REGULAR, 2};
+    sensors[3]   =   (struct Sensor){"THERMISTOR 2", THERMISTOR_REGULAR, 3};
+    sensors[4]   =   (struct Sensor){"THERMISTOR 3", THERMISTOR_REGULAR, 4};
+    sensors[5]   =   (struct Sensor){"THERMISTOR 4", THERMISTOR_REGULAR, 5};
+    sensors[6]   =   (struct Sensor){"THERMISTOR 5", THERMISTOR_REGULAR, 6};
+    sensors[7]   =   (struct Sensor){"THERMISTOR 6", THERMISTOR_REGULAR, 7};
+    sensors[8]   =   (struct Sensor){"THERMISTOR 7", THERMISTOR_REGULAR, 8};
+    sensors[9]   =   (struct Sensor){"THERMISTOR 8", THERMISTOR_REGULAR, 9};
+    sensors[10]  =   (struct Sensor){"LEM 1", LEM, 10};
+    sensors[11]  =   (struct Sensor){"LEM 2", LEM, 11};
+    sensors[12]  =   (struct Sensor){"VTAPS 1", VTAPS, 12};
+    sensors[13]  =   (struct Sensor){"VTAPS 2", VTAPS, 13};
+    sensors[14]  =   (struct Sensor){"VTAPS 3", VTAPS, 14};
+    sensors[15]  = (struct Sensor){"PRESSURE SENSOR", PRESSURE, 0};
       
     int i;
     while (1) {   
