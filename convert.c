c90a7330e2c18bff45cefc9c3c129078a91d3431
Jonathan Ta <Jonathan Ta>
2016-10-04 15:07:30 -0500

    Small bug changes, comments added to clear up code.

--- convert.c
+++ convert.c
@@ -4,15 +4,17 @@
  * Converts a given integer to a char* array of characters containing the binary
  * conversion.
  * @param input
- * @return 
+ * @return char* of size 32
  */
 char* convertIntToBinary(int input)
 {
+    //initialize variables
     int x;
     int remainder;
-    int digit = 2;
-    char* ptr = (char*) malloc(32);
+    int digit = 2;                  //represents which digit the function is calculating
+    char* ptr = (char*) malloc(32); //allocation of array
     
+    //algorithm utilizes the modulus function and remainder to compute the binary number
     for( x = MAX-32; input != 0; x--)
     {
         remainder = input%digit;
@@ -30,16 +32,26 @@ char* convertIntToBinary(int input)
     return ptr;
 }
 
+/**
+ * Converts a given binary char* array to a hex char* array.
+ * @param binary
+ * @return char* of size 8
+ */
 char* convertBinaryToHex(char* binary)
 {
+    //initialize variables
     int x;
-    int counter = 8;
+    int counter;
     int hex;
     char* ptr = malloc(8);
     
-    for(x=MAX-1;x>=0;x=x-4)
+    for(x = MAX-1;x >= 0;x = x - 4)
     {
+        counter = ((x+1)/4)-1;
+        //essentially adds up the binary number
         hex = atoi(binary+x-3)*8 + atoi(binary+x-2)*4 + atoi(binary+x-1)*2 + atoi(binary+x);
+        
+        //switch statement for the other hex values, could use array;
         switch( hex )
         {
             case 15:
@@ -63,11 +75,15 @@ char* convertBinaryToHex(char* binary)
             default:
                 *(ptr+counter) = (char)hex;
         }
-        counter --;
     }
     return ptr;
 }
 
+/**
+ * Converts a given integer to a hex char* array using the given functions above.
+ * @param input
+ * @return char* of size 8
+ */
 char* convertIntToHex(int input)
 {
     return convertBinaryToHex(convertIntToBinary(input));
