24761ad88b4e1eb53d475d21e2e2744b17c7c770
echan7 <echan7@wisc.edu>
2016-10-18 14:57:44 -0500

    Merge remote-tracking branch 'refs/remotes/origin/master' into VacuumDAQ
    # Conflicts:
    #	ADC.c
    #	ADC.h
    #	I2C.c
    #	I2C.h
    #	VL6180X.c
    #	VL6180X.h
    #	main.c
    #	main.h
    #	nbproject/configurations.xml

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <xc.h>
 #include "main.h"
 
-#define MAX_LENGTH          100
-#define NUM_SENSORS         17
-#define THERMISTOR_REGULAR  1
-#define THERM_REGULAR_SCALE 1111
-#define THERMISTOR_MOTOR    2
-#define THERM_MOTOR_SCALE   1111
-#define LEM                 3
-#define LEM_SCALE           1111
-#define VTAPS               4
-#define VTAPS_SCALE         1111
-#define PRESSURE            5
-#define PROX                6
-#define RAW_VALUE           0
+#define MAX_LENGTH              100
+#define NUM_SENSORS             17
+#define THERMISTOR_REGULAR      1
+
+#define THERMISTOR_MOTOR        2
+
+#define VTAPS                   4
+#define VTAPS_SCALE             140.0/3.3
+#define PRESSURE                5
+#define PROX                    6
+#define RAW_VALUE               0
+
+//resistor values may need to be changed when we measure each one 
+double resistorValues[]={3913,3913,1458,1458,1458,1458,1458,1458,1458,1458};
 
 /* having a sensors struct should make formatting to a csv easier since we
  can just write a for loop to output all the data. If needed, let pin1 be 
@@ -32,18 +33,20 @@ void initializers(void) {
     ADCinit();
     initVL(VL_ADDRESS);
 }
-
+double voltDiv(double value,int pin){
+    //voltage divider
+    double value=resistorValues[pin]*1/(value/3.3-1);
+   // need loop up table to get from ohms to temperature
+    return value;
+}
 /* In order to make this more modular */
 double readSensor(struct Sensor* sensor) {
     switch(sensor->type) {
         case THERMISTOR_REGULAR :
-            return readADC(sensor->pin)*THERM_REGULAR_SCALE;
+            return voltDiv(readADC(sensor->pin),sensor->pin);
             break;
         case THERMISTOR_MOTOR:
-            return readADC(sensor->pin)*THERM_MOTOR_SCALE;
-            break;
-        case LEM :
-            return readADC(sensor->pin)*LEM_SCALE;
+            return voltDiv(readADC(sensor->pin), sensor->pin);
             break;
         case VTAPS :
             return readADC(sensor->pin)*VTAPS_SCALE;
@@ -111,13 +114,11 @@ int main(void) {
     sensors[7]   =   (struct Sensor){"THERMISTOR 6", THERMISTOR_REGULAR, 7};
     sensors[8]   =   (struct Sensor){"THERMISTOR 7", THERMISTOR_REGULAR, 8};
     sensors[9]   =   (struct Sensor){"THERMISTOR 8", THERMISTOR_REGULAR, 9};
-    sensors[10]  =   (struct Sensor){"LEM 1", LEM, 10};
-    sensors[11]  =   (struct Sensor){"LEM 2", LEM, 11};
-    sensors[12]  =   (struct Sensor){"VTAPS 1", VTAPS, 12};
-    sensors[13]  =   (struct Sensor){"VTAPS 2", VTAPS, 13};
-    sensors[14]  =   (struct Sensor){"VTAPS 3", VTAPS, 14};
-    sensors[15]  =   (struct Sensor){"PRESSURE SENSOR", PRESSURE, 0};
-    sensors[16]  =   (struct Sensor){"PROXIMITY SENSOR", PROX, 0};
+    sensors[10]  =   (struct Sensor){"VTAPS 1", VTAPS, 10};
+    sensors[11]  =   (struct Sensor){"VTAPS 2", VTAPS, 11};
+    sensors[12]  =   (struct Sensor){"VTAPS 3", VTAPS, 12};
+    sensors[13]  =   (struct Sensor){"PRESSURE SENSOR", PRESSURE, 0};
+    sensors[14]  =   (struct Sensor){"PROXIMITY SENSOR", PROX, 0};
       
     int i;
     char buffer[150];
@@ -137,45 +138,4 @@ int main(void) {
     return 0;
 }
 
-  /*
-   * 
-   *    for (i = 0; i < NUM_ANALOG_INPUTS; i++) {
-        analogInputs[i] = 0;
-        }
-       
-        char reading[100];
-        char base []= "Reading 1 ";
-
-        analogInput1=readADC(0);
-        analogInput2=readADC(1);
-        analogInput3=readADC(2);
-        analogInput4=readADC(3);
-        analogInput5=readADC(4);
-        analogInput6=readADC(5);
-        analogInput7=readADC(6);
-        analogInput8=readADC(7);
-        analogInput9=readADC(8);
-        analogInput10=readADC(9);
-        analogInput11=readADC(10);
-        analogInput12=readADC(11);
-        analogInput13=readADC(12);
-        analogInput14=readADC(13);
-        analogInput15=readADC(14);
-       
-        sprintf(reading, "Reading 1: %d \n Reading 2: %d \n Reading 3: %d \n "
-                "Reading 4: %d \n Reading 5: %d \n Reading 6: %d \n Reading 7: "
-                "%d \n Reading 8:  %d \n", (int)analogInput1, (int)analogInput2,
-                (int)analogInput3, (int)analogInput4, (int)analogInput5, 
-                (int)analogInput6, (int)analogInput7, (int)analogInput8);
-        
-        print(reading);
-        sprintf( reading, "Reading 9: %d \n Reading 10: %d \n Reading 11: %d \n"
-                " Reading 12:  %d \n Reading 13: %d \n Reading 14: %d \n Reading"
-                " 15: %d \n", (int)analogInput9, (int)analogInput10, 
-                (int)analogInput11, (int)analogInput12, (int)analogInput13, 
-                (int)analogInput14, (int)analogInput15);
-        print(reading);
-        
-        println("");
-       
-        delay(1000, MILLI); */
+  
