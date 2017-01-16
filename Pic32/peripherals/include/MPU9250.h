bd7779c355f3449be2498e2d0fc30084ea65e66a
Nick Beckwith <nbeckwith2@wisc.edu>
2017-01-15 19:59:18 -0600

    forgot to name a field...

--- Pic32/peripherals/include/MPU9250.h
+++ Pic32/peripherals/include/MPU9250.h
@@ -1,13 +1,8 @@
 #ifndef _MPU9250__H__
 #define _MPU9250__H__
 
-
-
-#endif
-
-/******************************************************************************/
-/*                Structs, Enums and Compiler Directives                      */
-/******************************************************************************/
+#include "../../drivers/include/I2C.h"
+#define MPU_ADDRESS     0x68
 
 typedef union {
     // if i'm right, pos_z is the msb bit.
@@ -38,7 +33,7 @@ typedef union {
             signed X_coord:16;
         };
     };
-    unsigned long long:48; 
+    unsigned long long Raw_coord:48; 
 } COORD_VECTOR;
 
 // Functions
@@ -47,3 +42,5 @@ void MPUread(void);
 void MPUgetPos(COORD_VECTOR* pos);
 void MPUgetVel(COORD_VECTOR* vel);
 void MPUgetAcc(COORD_VECTOR* acc);
+
+#endif
