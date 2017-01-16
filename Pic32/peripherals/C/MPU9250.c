a67ae17cfc66a65d82f924f808f2290ecc7b1e8f
Nick Beckwith <nbeckwith2@wisc.edu>
2017-01-15 19:05:40 -0600

    Added COORD_VECTOR and interface
    COORD_VECTOR stores pos,vel or acc in a way that splits up the msB and
    lsB to make CAN easier. Added functions: init, read, getPos... getAcc

--- Pic32/peripherals/C/MPU9250.c
+++ Pic32/peripherals/C/MPU9250.c
@@ -1,3 +1,17 @@
 #include "../include/MPU9250.h"
 
-
+void MPUinitialize(void) {
+    
+}
+void MPUread(void) {
+    
+}
+void MPUgetPos(COORD_VECTOR* pos) {
+    
+}
+void MPUgetVel(COORD_VECTOR* vel) {
+    
+}
+void MPUgetAcc(COORD_VECTOR* acc) { 
+    
+}
--- Pic32/peripherals/include/MPU9250.h
+++ Pic32/peripherals/include/MPU9250.h
@@ -5,3 +5,45 @@
 
 #endif
 
+/******************************************************************************/
+/*                Structs, Enums and Compiler Directives                      */
+/******************************************************************************/
+
+typedef union {
+    // if i'm right, pos_z is the msb bit.
+    struct {
+        // pos_z
+        union {
+            struct {
+                signed Z_coord_msb:8;
+                unsigned Z_coord_lsb:8;
+            };
+            signed Z_coord:16;
+        };       
+        // pos_y
+        union {
+            struct {
+                signed Y_coord_msb:8;
+                unsigned Y_coord_lsb:8;
+            };
+            signed Y_coord:16;
+        };
+        // pos_x
+        union {
+            struct {
+                // is this order right?
+                signed X_coord_msb:8;
+                unsigned X_coord_lsb:8;
+            };
+            signed X_coord:16;
+        };
+    };
+    unsigned long long:48; 
+} COORD_VECTOR;
+
+// Functions
+void MPUinitialize(void);
+void MPUread(void);
+void MPUgetPos(COORD_VECTOR* pos);
+void MPUgetVel(COORD_VECTOR* vel);
+void MPUgetAcc(COORD_VECTOR* acc);
